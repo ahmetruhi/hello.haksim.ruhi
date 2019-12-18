da1ee57d93368dbf0a70e9277210ac2c4c1075f1
ZehraOzdemir <zehraz480@windowslive.com>
2019-12-18 11:02:51 +0300

    Add project files.

---
+++ hello.haksim.ruhi/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello.haksim.ruhi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sayac=0;
+            //while(sayac<5)
+            //{
+            //    Console.WriteLine("Gazi");
+            //    sayac++;
+            //}
+
+
+
+            //int toplam = 0;
+            //while (true)
+            //{
+            //    Console.WriteLine("Sayı Giriniz:");
+            //    int sayi = int.Parse(Console.ReadLine());
+            //    toplam += sayi;
+            //    if (sayi == 0)
+            //    {
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine($"Girdiğiniz Sayları Toplamı:{toplam}");
+
+
+
+            //Console.WriteLine("Bir sayı giriniz:");
+            //int sayi = int.Parse(Console.ReadLine());
+            //int toplam = 0;
+            //while(sayi!=0)
+            //{
+            //    toplam += sayi;
+            //    Console.WriteLine("Bir sayı giriniz:");
+            //    sayi = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine($"Girdiğiniz sayıların toplamı:{toplam}"); (((Console.WritLine'ı tekrar tekrar yazmaya gerek yok)))
+
+
+            int toplam = 0;
+            int sayi;
+            do
+            {
+                Console.WriteLine("Sayı giriniz:");
+                sayi = int.Parse(Console.ReadLine());
+                toplam += sayi;
+            } while (sayi != 0);
+            Console.WriteLine($"Girdiğiniz sayıları toplamı:{toplam}");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
